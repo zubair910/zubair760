9a62f89786b7c6a9ce5e0200922f3b3c345dbfdb
unknown <zubairjava123@gmail.com>
2021-12-04 08:35:04 +0500

    Work Complete

---
+++ app.js
@@ -0,0 +1,20 @@
+  
+ function names(){
+     var mainContent = document.getElementById("mainContent")
+     names.innerHTML
+ }
+
+ function moo(){
+     var render = document.getElementById("render")
+     document.write(render)
+    
+     
+ }
+
+
+ function foo(){
+    var content = document.getElementById("content")
+    document.write(content)
+   
+    
+}
